59d31745b973743b68bf4f35696cd80e7b5ccd74
Varsha-Arun <92varshaan@gmail.com>
2020-04-16 08:47:07 +0530

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -1,23 +1,25 @@
-apt-get update
+echo "echo.sh"
 
-apt-get install sudo
+# apt-get update
 
-sudo apt install bash-completion
+# apt-get install sudo
 
-sudo apt-get install sed
+# sudo apt install bash-completion
 
-sudo apt-get install coreutils
+# sudo apt-get install sed
 
-sudo apt-get install -y jq
+# sudo apt-get install coreutils
 
-sudo apt-get install -y gawk
+# sudo apt-get install -y jq
 
-sudo apt install -y git
+# sudo apt-get install -y gawk
 
-sudo apt-get install gettext
+# sudo apt install -y git
 
-sudo apt install curl
+# sudo apt-get install gettext
 
-sudo apt-get install cl-base64
+# sudo apt install curl
 
-sudo apt-get install openssl
+# sudo apt-get install cl-base64
+
+# sudo apt-get install openssl
